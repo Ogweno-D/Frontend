aa190367ab2e7974788677688d876b8b8e9ca170
Ogweno-D <dennisogwene@gmail.com>
2026-07-23 11:40:17 +0300

    Remove unused HTML, CSS, and image files; add new task.html and task.css for improved structure and styling

---
+++ Javascript/hello.js
@@ -0,0 +1,27 @@
+
+//  Keywords that are reserved as identifiers or default functions
+
+// Variables
+
+// const _score = 3;
+
+// // var ray = 5;
+
+// console.log(_score)
+
+if(true){
+    var x =5;
+}
+console.log(x);
+
+// data types
+
+// Simple functions
+
+// Conditional Statements
+
+// Loops
+
+// Function (Complex functions)
+
+//  Data Structures
